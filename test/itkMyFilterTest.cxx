24e0139ddf70ea277e2ddb81fed64adb0a8b5cfa
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2018-11-02 19:41:52 -0400

    Update latest ITKModuleTemplate
    Remove some placeholders

--- test/itkMyFilterTest.cxx
+++ test/itkMyFilterTest.cxx
@@ -16,12 +16,42 @@
  *
  *=========================================================================*/
 
-#include "itkNormalDistributionImageSource.h"
+#include "itkMyFilter.h"
 
+#include "itkCommand.h"
 #include "itkImageFileWriter.h"
 #include "itkTestingMacros.h"
 
-int itkNormalDistributionImageSourceTest( int argc, char * argv[] )
+namespace{
+class ShowProgress : public itk::Command
+{
+public:
+  itkNewMacro( ShowProgress );
+
+  void
+  Execute( itk::Object* caller, const itk::EventObject& event ) override
+  {
+    Execute( (const itk::Object*)caller, event );
+  }
+
+  void
+  Execute( const itk::Object* caller, const itk::EventObject& event ) override
+  {
+    if ( !itk::ProgressEvent().CheckEvent( &event ) )
+      {
+      return;
+      }
+    const auto* processObject = dynamic_cast< const itk::ProcessObject* >( caller );
+    if ( !processObject )
+      {
+      return;
+      }
+    std::cout << " " << processObject->GetProgress();
+  }
+};
+}
+
+int itkMyFilterTest( int argc, char * argv[] )
 {
   if( argc < 2 )
     {
@@ -32,26 +62,32 @@ int itkNormalDistributionImageSourceTest( int argc, char * argv[] )
     }
   const char * outputImageFileName  = argv[1];
 
-  const unsigned int                         Dimension = 2;
-  typedef float                              PixelType;
-  typedef itk::Image< PixelType, Dimension > ImageType;
+  const unsigned int Dimension = 2;
+  using PixelType = float;
+  using ImageType = itk::Image< PixelType, Dimension >;
 
-  typedef itk::NormalDistributionImageSource< ImageType > DistributionSourceType;
-  DistributionSourceType::Pointer distributionSource = DistributionSourceType::New();
+  using FilterType = itk::MyFilter< ImageType, ImageType >;
+  FilterType::Pointer filter = FilterType::New();
 
-  EXERCISE_BASIC_OBJECT_METHODS( distributionSource, NormalDistributionImageSource , GenerateImageSource );
+  EXERCISE_BASIC_OBJECT_METHODS( filter, MyFilter, ImageToImageFilter );
 
-
+  // Create input image to avoid test dependencies.
   ImageType::SizeType size;
   size.Fill( 128 );
-  distributionSource->SetSize( size );
+  ImageType::Pointer image = ImageType::New();
+  image->SetRegions( size );
+  image->Allocate();
+  image->FillBuffer(1.1f);
 
-  std::cout << distributionSource << std::endl;
+  ShowProgress::Pointer showProgress = ShowProgress::New();
+  filter->AddObserver( itk::ProgressEvent(), showProgress );
+  filter->SetInput(image);
 
   typedef itk::ImageFileWriter< ImageType > WriterType;
   WriterType::Pointer writer = WriterType::New();
   writer->SetFileName( outputImageFileName );
-  writer->SetInput( distributionSource->GetOutput() );
+  writer->SetInput( filter->GetOutput() );
+  writer->SetUseCompression(true);
   try
     {
     writer->Update();
