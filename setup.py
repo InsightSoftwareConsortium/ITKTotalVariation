b3564d7169c89d1264b5a70c1d320812187bfd68
Matt McCormick <matt.mccormick@kitware.com>
2021-02-15 14:52:59 -0500

    DOC: Update setup.py download_url

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author_email='pablo.hernandez.cerdan@outlook.com',
     packages=['itk'],
     package_dir={'itk': 'itk'},
-    download_url=r'https://github.com/phcerdan/ITKTotalVariation',
+    download_url=r'https://github.com/InsightSoftwareConsortium/ITKTotalVariation',
     description=r'TotalVariation algorithms, providing a wrap for the external project: https://github.com/albarji/proxTV',
     long_description='ITK is an open-source, cross-platform library that provides developers with an extensive suite of software tools for image analysis. Developed through extreme programming methodologies, ITK employs leading-edge algorithms for registering and segmenting multidimensional scientific images.',
     classifiers=[
